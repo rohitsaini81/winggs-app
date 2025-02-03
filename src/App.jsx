a1930d76554a9923d23d423be604f5bc81e43906
rohit <rohit@kali.kali>
2025-02-03 11:12:18 +0530

    Footer template added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./compt/Footer"
 import Highilights, { Highilight2 } from "./compt/Highilights"
 import Home, { HomeFooter } from "./compt/Home"
 import Nav from "./compt/Nav"
@@ -14,6 +15,9 @@ function App() {
     <Highilights />
     <Services />
     <Highilight2 />
+
+    <hr />
+    <Footer />
     </>
   )
 }
